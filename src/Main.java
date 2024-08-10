d00e0973d791f0c2af1cd0d04521129276f4a975
Raktbastr <raktbastr@halfheart.net>
2024-08-09 21:25:28 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float result = 0;
+        String anstring;
+        float ans = 0;
+
+        while (true) {
+            System.out.println("Symbols: +, -, *, /");
+            System.out.println("'ans' for previous answer");
+            Scanner scan1 = new Scanner(System.in);
+            System.out.print("Input: ");
+            String input1 = scan1.nextLine();
+            Scanner scan2 = new Scanner(System.in);
+            System.out.print("Operator: "+input1+" ");
+            String oper = scan2.nextLine();
+            Scanner scan3 = new Scanner(System.in);
+            System.out.print("Input: "+input1+" "+oper+" ");
+            String input2 = scan3.nextLine();
+            System.out.println(" ");
+            
+            float first;
+            float second;
+
+            if (input1.equalsIgnoreCase("ans")) {
+                first = ans;
+            } else {
+                first=Integer.parseInt(input1);
+            }
+            if (input2.equalsIgnoreCase("ans")) {
+                second = ans;
+            } else {
+                second=Integer.parseInt(input2);
+            }
+
+            if (oper.contains("+")) {
+                result = first + second;
+            } else if (oper.contains("*")) {
+                result = first * second;
+            } else if (oper.contains("/")) {
+                result = first / second;
+            }
+            ans = result;
+            anstring = Float.toString(result);
+            System.out.println("Answer: " + anstring);
+        }
+    }
+}
